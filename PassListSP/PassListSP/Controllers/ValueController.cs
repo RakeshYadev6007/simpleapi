bcedb3fe4178e7f05cb7e1e0e9c68830b4ff3bdf
RakeshYadev6007 <138871583+RakeshYadev6007@users.noreply.github.com>
2023-07-08 15:10:33 +0530

    ID Pass Value

---
+++ PassListSP/PassListSP/Controllers/ValueController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using PassListSP.Model;
+
+namespace PassListSP.Controllers
+{
+    [ApiController]
+    [Route("api/values")]
+    public class ValueController: ControllerBase
+    {
+        private string? ConnectionString;
+
+        public ValueController(IConfiguration configuration)
+        {
+            ConnectionString = configuration.GetConnectionString("DefeultConnection");       
+        
+        }
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Value>> Get([FromQuery] int[] ids)
+        {
+            var result = new List<Value>();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            
+            {
+                conn.Open();
+                using (SqlCommand command = new SqlCommand("Values_GetValues", conn))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    var dt = new DataTable();
+                    dt.Columns.Add("Id", typeof(int));
+                    foreach (var id in ids)
+                    {
+                        dt.Rows.Add(id);
+                    
+                    }
+                    var parameter = command.Parameters.AddWithValue("ListIds", dt);
+                    parameter.SqlDbType = SqlDbType.Structured;
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        result.Add(new Value()
+                        {
+                            Id = int.Parse(reader["Id"].ToString()!),
+                            Text = reader["Value"].ToString()!
+                        });
+                           
+                    }
+
+                }
+                conn.Close();
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ PassListSP/PassListSP/Value.cs
@@ -0,0 +1,9 @@
+﻿namespace PassListSP.Model
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string? Text { get; set; }
+    }
+}
